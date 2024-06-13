71a448d3372b2a109b197e8c951360beb4e963b4
ahmadrezahemmat <hemmatahmadreza@gmail.com>
2024-06-14 01:05:03 +0330

    update style login

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,10 +1,28 @@
-import MyNavbar from "../../components/navbar/Navbar"
-function Login(){
-    return(
-        <div>
-            <MyNavbar/>
-            <h1>login page</h1>
+import MyNavbar from "../../components/navbar/Navbar";
+import "./Login.css";
+function Login() {
+  return (
+    <>
+      <MyNavbar />
+      <div className="login-page">
+        <div className="form">
+          <form className="register-form">
+            <input type="text" placeholder="name" />
+            <input type="password" placeholder="password" />
+            <input type="text" placeholder="email address" />
+            <button>create</button>
+            <p className="message">
+              Already registered? <a href="#">Sign In</a>
+            </p>
+          </form>
+          <form className="login-form">
+            <input type="text" placeholder="username" />
+            <input type="password" placeholder="password" />
+            <button>login</button>
+          </form>
         </div>
-    )
+      </div>
+    </>
+  );
 }
-export default Login
+export default Login;
