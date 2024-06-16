23a168c1bc997049ce6ee62cb6d2f81982a37963
ahmadrezahemmat <hemmatahmadreza@gmail.com>
2024-06-16 12:43:13 +0330

    update login

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,24 +1,43 @@
+import { useState } from "react";
 import MyNavbar from "../../components/navbar/Navbar";
 import "./Login.css";
+import Swal from "sweetalert2";
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = () => {
+    if (username == "hemmatahmad374@gmail.com" && password == "Ahmad70a@") {
+      console.log("welcome");
+    } else {
+      Swal.fire({
+        title: "Error!",
+        text: "Do you want to continue",
+        icon: "error",
+        confirmButtonText: "Close",
+      });
+    }
+  };
+
   return (
     <>
       <MyNavbar />
       <div className="login-page">
         <div className="form">
-          <form className="register-form">
-            <input type="text" placeholder="name" />
-            <input type="password" placeholder="password" />
-            <input type="text" placeholder="email address" />
-            <button>create</button>
-            <p className="message">
-              Already registered? <a href="#">Sign In</a>
-            </p>
-          </form>
           <form className="login-form">
-            <input type="text" placeholder="username" />
-            <input type="password" placeholder="password" />
-            <button>login</button>
+            <input
+              onChange={(e) => setUsername(e.target.value)}
+              type="text"
+              placeholder="username"
+            />
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              placeholder="password"
+            />
+            <button type="button" onClick={submitHandler}>
+              login
+            </button>
           </form>
         </div>
       </div>
